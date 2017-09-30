72562116cf06ebd0b505809fa3de7281e5c2b053
Andrew McConville <andrew@andrewmcconville.com>
2017-09-29 23:07:03 -0500

    resume

--- src/app/about/resume/resume.component.ts
+++ src/app/about/resume/resume.component.ts
@@ -3,7 +3,7 @@ import { PortfolioUIService } from '../../portfolio-ui.service';
 
 @Component({
     templateUrl: './resume.component.html',
-    //styleUrls: ['./resume.component.scss']
+    styleUrls: ['./resume.component.scss']
 })
 export class ResumeComponent implements OnInit {
     scrollbarWidth: number;
