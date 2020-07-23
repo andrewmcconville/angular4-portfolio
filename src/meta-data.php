bf36968bf28065480c17b54a496ae9e282eb85fe
Andrew McConville <andrew@andrewmcconville.com>
2020-07-23 10:12:00 -0500

    updates metadata

--- src/meta-data.php
+++ src/meta-data.php
@@ -60,7 +60,7 @@
         ],
         "" => [
             "title" => "Andrew McConville - Portfolio",
-            "description" => "Hi, I’m Andrew McConville; I work with product & design on creating user workflows, wireframes and adhering to user interface consistency.",
+            "description" => "UX professional with a background in B2B SaaS applications.",
             "image" => "/assets/about/home-social-media.png",
             "canonical" => "/",
             "date" => "2018-03-02T18:00:00-05:00"
